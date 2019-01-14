8906304d022200d6ddae9a828ab4b07b9c6d57d8
z003uutr <z003uutr@md1u6cgc.ad001.siemens.net>
2019-01-14 11:33:22 +0530

    Initial commit of project.

---
+++ Learning/src/com/days/Program1.java
@@ -0,0 +1,21 @@
+package com.days;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SalesData data=new SalesData();
+				
+		displayGreeting();
+		data.Display();		
+
+	}
+	
+	private static void displayGreeting()
+	{
+		System.out.println("Hello Happy Sales People");
+		System.out.println("This App shows the sales data");
+	}
+
+}
---
+++ Learning/src/com/days/SalesData.java
@@ -0,0 +1,10 @@
+package com.days;
+
+public class SalesData {
+
+	public void Display()
+	{
+		System.out.println("Welcome to Java, Good Morning have a great day");
+		
+	}
+}
